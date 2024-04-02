8605ba1b9628d1cd8f2a2983dff9cf52f4ff6081
anushka-bhagat <161180963+anushka-bhagat@users.noreply.github.com>
2024-04-02 19:16:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Movie Reccomendation System")
+st.title("🌟Movie Reccomendation System🌟")
+st.markdown("Feeling indecisive? Let me handle that! Select your favorite genres and a preferred rating, then hit generate! 🍿🌟")
+
 #path = 'MovieLens 20M Dataset/'
 
 #import datatsets
@@ -26,6 +28,7 @@ for genre in movie_rating['genres']:
   genre_list = genre.split("|")
   genres.update(genre_list)
 
+st.header("Filters:")
 selected_genres = st.multiselect("Select Genre(s)", genres)
 selected_rating = float(st.select_slider('Select a Rating', ['0', '0.5', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5']))
 generate_button = st.button('Generate!')
@@ -38,14 +41,19 @@ def display_recommendations(df, selected_genres, selected_rating):
         selected_rating = float(selected_rating)
         
         filtered_movies = df[(df[selected_genres].sum(axis=1) == len(selected_genres)) & (df['rating'] >= selected_rating)]
+        
         if not filtered_movies.empty:
             st.balloons()
-            st.write(filtered_movies['title'])
+            
+            output = filtered_movies[['title','genres','rating']].style.format({'rating': '{:.1f}'}).hide_index()
+            st.table(output)
         else:
             st.error("No Novies Found :(")
 
+    else:
+        st.warning("Please select a genre and rating to proceed...")
 
 if generate_button:
     display_recommendations(movie_rating, selected_genres, selected_rating)
    
-        
+          
